633a8917414607e3569a9f58c3a291dea376e87e
Parth Parikh <parthparikh28@gmail.com>
2022-02-09 19:31:51 +0530

    Changed URL

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ from zipfile import ZipFile
 
 
 ASSETS_PATH = './Assets/'
-ASSETS_URL = 'https://github.com/ptp28/eYSRC21-FinalsGames/blob/Assets/12_Assets.zip?raw=true'
+ASSETS_URL = 'https://github.com/ptp28/eYSRC21-FinalsGames/blob/Assets/12_Sudoku_Assets.zip?raw=true'
 
 def extract(filename):
     # opening the zip file in READ mode
